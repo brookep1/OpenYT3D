19a24d947742e5377dc8ec8a0cd57c2c4226de22
Brooke Patterson <brookep1@aol.com>
2015-07-26 07:24:05 -0400

    Lot's of updates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 #Open Youtube in 3DVision Stereo Video Player
 https://github.com/brookep1/OpenYT3D
 
-This batch script can be used to play 3D Youtube videos on the standard NVidia 3DVision Player
+This helper batch script can be used to play 3D Youtube videos on the standard NVidia 3DVision Player
 
 ##Features:
 
@@ -9,7 +9,9 @@ You give it the video URL or ID and streams directly from Youtube, you don't hav
 
 The video does not have to be tagged in Youtube as a 3D video first. It will accept and play any Half SBS video that is posted.
 
-No need for an HTML5 browser. No Flash. And no special plugins necessary. It does not use your browser at all.
+No need for an HTML5 browser. No Flash. And no special plugins necessary. It does not use your browser at all. 
+
+Also no fancy .NET libraries or similar. Fancy GUI's are old school. Simple text screens are the future.
 
 ##Requires:
 
@@ -20,13 +22,19 @@ http://www.nvidia.com/object/3d-vision-main.html
 
 **NVidia 3D Vision Video Player** -- rebranded version of stereoplayer.exe
 As of this scripting V1.7.5 is the latest
+
 Download: http://www.nvidia.com/object/3d-vision-video-player-1.7.5-driver.html
 
 ###3dtv.at Stereoscopic Player -- trial or pay
 http://www.3dtv.at/Products/Player/Index_en.aspx
+
 Download: http://www.3dtv.at/Downloads/Index_en.aspx
 
 ###rg3's Youtube Downloader -- (youtube-dl.exe)
+
 https://github.com/rg3/youtube-dl
+
 Download Windows .exe: https://yt-dl.org/latest/youtube-dl.exe
 
+**The youtube-dl.exe tool can do more than just YouTube**. Check out it's notes for other sites it can retrieve fun.
+Currently this batch script only supports pulling 3D videos from YT. Thanks all who work on this tool.
--- openyt3d.bat
+++ openyt3d.bat
@@ -1,13 +1,29 @@
 @echo off
-
 SETLOCAL ENABLEEXTENSIONS EnableDelayedExpansion
+SET VER=0.1
+
+rem Left Todo
+rem file reorganize - depends for binary depot and temp for created files
+rem command line parameter passing
+rem player specific command line flag parameters
+rem Tasklist and Taskkill
+rem Powershell clipboard output helper
+rem clip2stereoplayer batch shortcut
+rem openyt3d-stereoplayer shorthand
+rem media2clip shorthand
+rem openyt3d-bino shorthand
+rem clip2bino batch shortcut
+rem playlist parser
+
 CLS
 rem ################ You can edit this stuff with the right directories and files ######
 rem It will check params
 rem Then it will check current path and these directories for the PLAYER
 SET NVDIR=%ProgramFiles(x86)%\NVIDIA Corporation\NVIDIA 3D Vision Video Player\
 SET PLAYERDIR=%ProgramFiles(x86)%\Stereoscopic Player\
+SET YTDLDIR=%ProgramFiles(x86)%\youtube-dl\
 SET PLAYER=StereoPlayer.exe
+SET YTDL=youtube-dl.exe
 
 rem Call the parameter override file if one exists
 IF EXIST params.ini (
@@ -21,105 +37,152 @@ IF EXIST params.ini (
 
 IF /I NOT "!NOTES!"=="SKIP" (
 	cls
+	echo HELLO TO YOU! 
+	echo.
+	echo This batch script will help you open a Youtube Video in your 3DVision PLAYER
+	echo You can find the latest version in GitHub here -- https://github.com/brookep1/OpenYT3D
+	echo.
 	echo ---------------
-	echo -- NOTES --    Edit params.ini to skip the notes
+	echo -- NOTES --    Edit the params.ini to permanently skip this
 	echo ---------------
 	type NOTES.txt
 	echo.
 	echo.
 	echo ---------------
+	echo Press CONTROL-C to discontinue
 	pause 
 	cls
 )
 
-if NOT EXIST youtube-dl.exe (
-	echo.
-	echo Missing Dependencies
-	echo see the README for more information
-	pause
-	GOTO EOF
+if EXIST !YTDL! (
+	rem youtube-dl in local directory
+	GOTO FINDPLAYER
 )
 
+if EXIST !YTDLDIR!!YTDL! (
+	rem youtube-dl in local directory
+	SET YTDL=!YTDLDIR!!YTDL!
+	GOTO FINDPLAYER
+)
+
+echo PROBLEM:  No youtube-dl.exe found
+echo see the README for more information
+pause
+GOTO EOF
+
+
+:FINDPLAYER
 rem check for existence of %PLAYER%
 IF EXIST !PLAYER! (
 	rem player in local directory
 	GOTO YOUTUBE
 )
 
-IF EXIST %NVDIR%!PLAYER! (
-	SET PLAYER=!%NVDIR%!PLAYER!!
+IF EXIST !NVDIR!!PLAYER! (
+	SET PLAYER=!NVDIR!!PLAYER!
 	GOTO YOUTUBE
 )
 
-IF EXIST %PLAYERDIR%!PLAYER! (
-    SET PLAYER=%PLAYERDIR%!PLAYER!
+IF EXIST !PLAYERDIR!!PLAYER! (
+    SET PLAYER=!PLAYERDIR!!PLAYER!
 	GOTO YOUTUBE
 )
+
 ECHO PROBLEM: No Stereo Player could be found. 
-GOTO BAD
+echo see the README for more information
+pause
+GOTO EOF
 
 :YOUTUBE
 rem CLS
 IF NOT "%1"=="" (
 	SET INPUT=%1
 ) ELSE (
-	ECHO --------------------------------------------------------------
+	ECHO version: %VER%
+	ECHO -------------------------------------------------------------
 	echo COPY The YouTube URL without any trailing items after the ID
-	echo or COPY just the 11 character ID
+	echo -- or COPY just the 11 character ID
 	ECHO --------------------------------------------------------------
 	echo.
 	ECHO --------------------------------------------------------------
-	echo Example -- this is the old NVidia video
-	echo https://www.youtube.com/watch?v=FpSR2xUc-CI
-	echo type "test" (no quotes^) to use it for a test
+	echo -- Example: FpSR2xUc-CI or https://www.youtube.com/watch?v=FpSR2xUc-CI
+	echo -- for YouTube URLs it must end with the video ID
+	echo.
+	echo -- Type "other" for other youtube-dl supported site URLs
+	echo --    VIMEO for example. Refer to youtube-dl docs for full list.
+	echo -- Type "playlist" to pick from your playlist.txt file
+	echo -- Type "dry" to show the video URL
+	echo -- Type "exit" to end the script
 	ECHO --------------------------------------------------------------
 	echo.
 	:ASK
-	SET /P INPUT="TYPE OR PASTE THE YOUTUBE OR URL -->  " || ECHO "Invalid Entry" && GOTO ASK
+	SET /P INPUT="YouTube URL or ID -->  " || ECHO "Invalid Entry" && GOTO ASK
 	echo.
 	if /I "!INPUT!"=="test" (
-		SET INPUT=FpSR2xUc-CI
-		SET CHOICE=best
+		SET URL=https://www.youtube.com/v/FpSR2xUc-CI
+		GOTO GET
 	)
-	if /I "!INPUT!"=="test2" (
-		SET INPUT=FpSR2xUc-CI
-		SET CHOICE=85
+	if /I "!INPUT!"=="other" (
+		SET /P INPUT="Other Video URL -->  " || ECHO "Invalid Entry" && GOTO ASK
+		SET URL=!INPUT!
+		GOTO GET
 	)
+	if /I "!INPUT!"=="dry" (
+		echo Doing a dry run.
+		SET /P INPUT="Other Video URL -->  " || ECHO "Invalid Entry" && GOTO ASK
+		echo -------- Media URL -------------
+		youtube-dl -g !INPUT!
+		echo -------- Formats ---------------
+		youtube-dl -F !INPUT!
+		echo ---------------------------------
+		SET URL=!INPUT!
+		pause
+		GOTO YOUTUBE
+	)
+
 )
 
-SET PREFIX=https://www.youtube.com/v/
-SET ID=%INPUT:~-11% 
-ECHO The YouTube ID is %ID%
-SET URL=%PREFIX%%ID%
+IF /I !INPUT:~1-4! == "http" (
+) ELSE (
+	SET PREFIX=https://www.youtube.com/v/
+	SET URL=!PREFIX!!ID!
+)
 
-rem getting available formats that are not only audio or video (DASH)
-
-IF /I "!FORMAT!"=="AUTO" (
-	SET CHOICE=best
+rem Don't care just make it go
+IF /I "!FORMAT!"=="AUTO" DO (
 	GOTO GET
 )
 
-pause
+rem getting available formats that are not DASH
+youtube-dl -F !URL!  > ytformats.txt || ECHO Does not appear to be a valid youtube URL or ID && GOTO BAD
+FOR %%A IN (ytformats.txt) DO set size=%%~zA
+
+rem There are no formats returned. This is probably not a YouTube URL
+IF NOT %%A GTR 1 DO (
+	FMT=""
+	ECHO There is only one video resolution available for this video
+	GOTO GET 
+)
+
 CLS
-ECHO Video ID: %ID%
+ECHO Video ID: !ID!
 ECHO. --------------------------------------------------------------
-ECHO ENTER ONE OF THESE AVAILABLE FORMAT CODES FROM THE FIRST COLUMN
-ECHO NOTE:The "(best)" tagged format might not be highest resolution
+ECHO ENTER ONE OF THE AVAILABLE FORMAT CODES FROM THE FIRST COLUMN
 ECHO. --------------------------------------------------------------
-ECHO. If the video does not play retry with a different format code
+ECHO. "(best)" may or may not be true.
 ECHO.
-youtube-dl -F %URL%  > ytformats.txt || ECHO Does not appear to be a valid youtube URL or ID && GOTO BAD
 TYPE ytformats.txt | find /v "DASH" | find /v "]"
 
 :Pick
 SET /P CHOICE="Type the format code -->  " || ECHO Invalid Entry Try Again && GOTO Pick
+FMT="-f "!CHOICE!
 
 rem retrieve the video URL for that format and forward it
 ECHO Retrieving direct URL for the video
 DEL playurl.txt
 
 :GET
-youtube-dl -f !CHOICE! -g %URL% > playurl.txt || ECHO COULD NOT GET YT URL && goto BAD
+!YTDL!!FMT! -g !URL! > playurl.txt || ECHO. && echo PROBLEM: COULD NOT GET YT VIDEO URL && goto BAD
 SET /P VID=<playurl.txt 
 
 :Play
@@ -129,8 +192,10 @@ SET FlAGS=-il:SideBySideLF -ihw -fp -ol:NVIDIA
 ECHO.
 ECHO.
 rem Using !PLAYER! to run !VID!
+ECHO Launching Video Player. Enjoy the show!
+START /W /I /B "" "!PLAYER!" %FLAGS% -url:"!VID!" || ECHO. && echo PROBLEM: COULD NOT START PLAYER OR VIDEO && goto BAD
+pause
 CLS
-START /W /I /B "" "!PLAYER!" %FLAGS% -url:"%VID%" || ECHO COULD NOT START PLAYER OR VIDEO && goto BAD
 ECHO.
 SET VID=""
 SET CHOICE=""
@@ -140,7 +205,7 @@ GOTO YOUTUBE
 
 :BAD
 ECHO.
-ECHO Something went wrong. Check for fixable batch issues from the console.
+ECHO. && echo Something went wrong. Ending the script now.
 ECHO.
 pause
 
